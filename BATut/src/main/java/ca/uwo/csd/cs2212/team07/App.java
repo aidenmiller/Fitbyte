15e40e3446ed2a118d420ef1fe2e82f2aa239f6f
Jue OuYang <jouyan2@uwo.ca>
2016-02-22 22:22:01 -0500

    Maven automation for team07

---
+++ BATut/src/main/java/ca/uwo/csd/cs2212/team07/App.java
@@ -0,0 +1,8 @@
+package ca.uwo.csd.cs2212.team07; 
+public class App {
+      public static void main ( String [] args ) {
+                System.out.println("Hello Maven World");
+		System.out.println("My username is team07");
+		}
+	}
+
