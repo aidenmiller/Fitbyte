911d46cf3d413493fd8dd83118e34fe1c3a3c179
Aiden Miller <amill84@uwo.ca>
2016-02-24 10:48:53 -0500

    Initial Commit- Base API features

---
+++ src/main/java/ca/uwo/csd/cs2212/team07/testapi.java
@@ -0,0 +1,14 @@
+package ca.uwo.csd.cs2212.FitbitTest;
+
+public class testapi{
+	
+	
+	public static void main(String[] args)
+	{
+		Api test = new Api();
+		System.out.println("Diggy dog");
+		test.test123();
+		
+	}
+	
+}
