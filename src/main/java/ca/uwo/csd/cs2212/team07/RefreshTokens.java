20404e3f5e6f37b70d46983a947c0cb0802eae70
Aiden Miller <amill84@uwo.ca>
2016-02-28 19:16:28 -0500

    Test

--- src/main/java/ca/uwo/csd/cs2212/team07/RefreshTokens.java
+++ src/main/java/ca/uwo/csd/cs2212/team07/RefreshTokens.java
@@ -6,10 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
+
 import com.github.scribejava.apis.FitbitApi20;
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.oauth.OAuthService;
